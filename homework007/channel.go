64ebbb45e739f6612310c76791a909d8169646fb
nrhirano <ryuuji.hirano@nishio-rent.co.jp>
2021-11-09 14:57:06 +0900

    feat: 第7回スクール宿題

--- homework007/channel.go
+++ homework007/channel.go
@@ -5,11 +5,19 @@ import (
 )
 
 func task3(s []int, c chan int) {
-
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
 }
 
 func Test_4() {
 	fmt.Println("homework007_4")
 	s := []int{1, 2, 3, 4}
 	// 第7回スクール 宿題4 チャネルを実装してください。
+	c := make(chan int)
+	go task3(s, c)
+	p := <-c
+	fmt.Println(p)
 }
